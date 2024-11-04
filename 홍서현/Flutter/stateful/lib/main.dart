6cf73611ba941c13b35714c461ec0846e5860656
xyz987164 <xyz987164@gmail.com>
2024-11-04 21:31:40 +0900

    5주차: Widget State 이해하기

---
+++ 홍서현/Flutter/stateful/lib/main.dart
@@ -0,0 +1,86 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const App());
+}
+
+class App extends StatefulWidget {
+  const App({super.key});
+
+  @override
+  State<App> createState() => _Appstate();
+}
+
+class _Appstate extends State<App> {
+  bool showTitle = true;
+
+  void toggleTitle() {
+    setState(() {
+      showTitle = !showTitle;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        theme: ThemeData(
+          textTheme: const TextTheme(
+            titleLarge: TextStyle(
+              color: Colors.red,
+            ),
+          ),
+        ),
+        home: Scaffold(
+            backgroundColor: const Color(0xFFF4EDDB),
+            body: Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  showTitle ? const MyLargeTitle() : const Text('nothing'),
+                  IconButton(
+                      onPressed: toggleTitle,
+                      icon: const Icon(Icons.remove_red_eye))
+                ],
+              ),
+            )));
+  }
+}
+
+class MyLargeTitle extends StatefulWidget {
+  const MyLargeTitle({
+    super.key,
+  });
+
+  @override
+  State<MyLargeTitle> createState() => _MyLargeTitleState();
+}
+
+class _MyLargeTitleState extends State<MyLargeTitle> {
+  @override
+  void initState() {
+    //초기화
+    //only one time, before b uild method
+    super.initState();
+    print('initstate!');
+  }
+
+  @override
+  void dispose() {
+    //취소
+    // TODO: implement dispose
+    super.dispose();
+    print('dispose!');
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    print('build!');
+    return Text(
+      'My Large Title',
+      style: TextStyle(
+        fontSize: 30,
+        color: Theme.of(context).textTheme.titleLarge?.color,
+      ),
+    );
+  }
+}
