1dd09107659511e54c8dcfec6b246d6b1c796889
yoojinche <chesunny917@gmail.com>
2024-11-20 17:20:54 +0900

    7주차 : Data Fetch 이해하기

---
+++ flutter_projects/webtoon_app/lib/services/api_service.dart
@@ -0,0 +1,28 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+import 'package:webtoon_app/models/webtoon_model.dart';
+
+class ApiService {
+  final String baseUrl = "https://webtoon-crawler.nomadcoders.workers.dev";
+  final String today = "today";
+
+  // api에 요청을 할 수 있었으면 좋겠음
+  // 그리고 pai가 반환한 json을 내 콘솔에 프린트하도록한다
+
+  Future<List<WebtoonModel>> getTodaysToon() async {
+    List<WebtoonModel> webtoonInstances = [];
+    final url = Uri.parse('$baseUrl/$today');
+    final response = await http.get(url);
+    if (response.statusCode == 200) {
+      final List<dynamic> webtoons = jsonDecode(response.body);
+      for (var webtoon in webtoons) {
+        // final toon = WebtoonModel.fromJson(webtoon);
+        // print(toon.title);
+        webtoonInstances.add(WebtoonModel.fromJson(webtoon));
+      }
+      return webtoonInstances;
+    }
+    throw Error();
+  }
+}
