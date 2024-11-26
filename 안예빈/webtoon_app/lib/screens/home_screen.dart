2dcad749945fcbbe991cd16f2290746bfffeba7e
YeBin0711 <ayb0711@sookmyung.ac.kr>
2024-11-26 17:44:02 +0900

    feat: waitForWebToons
    - Future 데이터를 불러와서 보여주는 첫 번째 방법으로 async/await을 사용한다.

--- 안예빈/webtoon_app/lib/screens/home_screen.dart
+++ 안예빈/webtoon_app/lib/screens/home_screen.dart
@@ -1,21 +1,45 @@
 import 'package:flutter/material.dart';
+import 'package:webtoon_app/models/webtoon_model.dart';
+import 'package:webtoon_app/services/api_service.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
 
   @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  List<WebtoonModel> webtoons = [];
+  bool isLoading = true;
+
+  void waitForWebToons() async {
+    webtoons = await ApiService.getTodaysToons();
+    isLoading = false;
+    setState(() {});
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    waitForWebToons();
+  }
+
+  @override
   Widget build(BuildContext context) {
+    print(webtoons);
+    print(isLoading);
     return Scaffold(
-      //screen을 위한 기본 레이아웃 설정 제공
       backgroundColor: Colors.white,
       appBar: AppBar(
         elevation: 2,
-        //앱바의 음영도
-        backgroundColor: Colors.green,
-        foregroundColor: Colors.white,
+        backgroundColor: Colors.white,
+        foregroundColor: Colors.green,
         title: const Text(
           "오늘의 웹툰",
-          style: TextStyle(fontSize: 24, fontWeight: FontWeight.w400),
+          style: TextStyle(
+            fontSize: 24,
+          ),
         ),
       ),
     );
