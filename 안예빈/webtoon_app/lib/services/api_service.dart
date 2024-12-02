2768742d6c3d354ad00795300ab6c7f2e42d7cae
YeBin0711 <ayb0711@sookmyung.ac.kr>
2024-12-02 21:46:38 +0900

    fix: api_service
    - 상세 정보가 출력이 안돼서 수정

--- 안예빈/webtoon_app/lib/services/api_service.dart
+++ 안예빈/webtoon_app/lib/services/api_service.dart
@@ -6,19 +6,17 @@ import 'package:webtoon_app/models/webtoon_episode_model.dart';
 import 'package:webtoon_app/models/webtoon_model.dart';
 
 class ApiService {
-  static const String baseUrl =
-      "https://webtoon-crawler.nomadcoders.workers.dev";
-  static const String today = "today";
+  static String baseURL = "https://webtoon-crawler.nomadcoders.workers.dev";
+  static String today = "today";
 
   static Future<List<WebtoonModel>> getTodaysToons() async {
     List<WebtoonModel> webtoonInstances = [];
-    final url = Uri.parse('$baseUrl/$today');
+    final url = Uri.parse('$baseURL/$today');
     final response = await http.get(url);
     if (response.statusCode == 200) {
-      final webtoons = jsonDecode(response.body);
+      final List<dynamic> webtoons = jsonDecode(response.body);
       for (var webtoon in webtoons) {
-        final instance = WebtoonModel.fromJson(webtoon);
-        webtoonInstances.add(instance);
+        webtoonInstances.add(WebtoonModel.fromJson(webtoon));
       }
       return webtoonInstances;
     }
@@ -26,25 +24,26 @@ class ApiService {
   }
 
   static Future<WebtoonDetailModel> getToonById(String id) async {
-    final url = Uri.parse("$baseUrl/$id");
+    final url = Uri.parse("$baseURL/$id");
     final response = await http.get(url);
     if (response.statusCode == 200) {
       final webtoon = jsonDecode(response.body);
-      WebtoonDetailModel.fromJson(webtoon);
+      return WebtoonDetailModel.fromJson(webtoon);
     }
     throw Error();
   }
 
-  static Future<List<WebtoonEpisodeModel>> getLatestEpisodesById(
+  static Future<List<WebtoonEpisodeModel>> getLatestEpisodeById(
       String id) async {
     List<WebtoonEpisodeModel> episodesInstances = [];
-    final url = Uri.parse("$baseUrl/$id/episodes");
+    final url = Uri.parse("$baseURL/$id/episodes");
     final response = await http.get(url);
     if (response.statusCode == 200) {
       final episodes = jsonDecode(response.body);
       for (var episode in episodes) {
         episodesInstances.add(WebtoonEpisodeModel.fromJson(episode));
       }
+      return episodesInstances;
     }
     throw Error();
   }
