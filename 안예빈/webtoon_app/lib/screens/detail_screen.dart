3dcdad87130359e957f736ce37353c5843e68c1c
YeBin0711 <ayb0711@sookmyung.ac.kr>
2024-12-02 21:45:45 +0900

    feat: detail screen UI

--- 안예빈/webtoon_app/lib/screens/detail_screen.dart
+++ 안예빈/webtoon_app/lib/screens/detail_screen.dart
@@ -25,7 +25,7 @@ class _DetailScreenState extends State<DetailScreen> {
   void initState() {
     super.initState();
     webtoon = ApiService.getToonById(widget.id);
-    episodes = ApiService.getLatestEpisodesById(widget.id);
+    episodes = ApiService.getLatestEpisodeById(widget.id);
   }
 
   @override
@@ -33,14 +33,15 @@ class _DetailScreenState extends State<DetailScreen> {
     return Scaffold(
       backgroundColor: Colors.white,
       appBar: AppBar(
+        centerTitle: true,
+        surfaceTintColor: Colors.white,
+        shadowColor: Colors.black,
         elevation: 2,
         backgroundColor: Colors.green,
         foregroundColor: Colors.white,
         title: Text(
           widget.title,
-          style: const TextStyle(
-            fontSize: 24,
-          ),
+          style: const TextStyle(fontSize: 24),
         ),
       ),
       body: Column(
@@ -57,20 +58,50 @@ class _DetailScreenState extends State<DetailScreen> {
                   width: 250,
                   clipBehavior: Clip.hardEdge,
                   decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(15),
-                    boxShadow: [
-                      BoxShadow(
-                        blurRadius: 15,
-                        offset: const Offset(10, 10),
-                        color: Colors.black.withOpacity(0.3),
-                      )
-                    ],
+                      borderRadius: BorderRadius.circular(15),
+                      boxShadow: [
+                        BoxShadow(
+                            blurRadius: 15,
+                            offset: const Offset(10, 10),
+                            color: Colors.black.withOpacity(0.5))
+                      ]),
+                  child: Image.network(
+                    widget.thumb,
+                    headers: const {'Referer': 'https://cosmic.naver.com'},
                   ),
-                  child: Image.network(widget.thumb),
                 ),
               ),
             ],
           ),
+          const SizedBox(
+            height: 25,
+          ),
+          FutureBuilder(
+              future: webtoon,
+              builder: (context, snapshot) {
+                if (snapshot.hasData) {
+                  return Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 50),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          snapshot.data!.about,
+                          style: const TextStyle(fontSize: 16),
+                        ),
+                        const SizedBox(
+                          height: 15,
+                        ),
+                        Text(
+                          '${snapshot.data!.genre} / ${snapshot.data!.age}',
+                          style: const TextStyle(fontSize: 16),
+                        ),
+                      ],
+                    ),
+                  );
+                }
+                return const Text("....");
+              })
         ],
       ),
     );
